ac42906a7da997aaf9f976dcb7a57f4822bc3f08
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-04-18 00:14:32 +0100

    [chore] initialize project with Next.js, TailwindCSS, and TypeScript

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    appDir: false
+  }
+}
+
+module.exports = nextConfig
