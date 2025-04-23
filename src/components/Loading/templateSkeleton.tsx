6237ceff9c13d9bcdf6fbe66e0fd9b09c1cf14bf
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-04-23 23:40:27 +0100

    [add] animate-pulse to skeleton elements

--- src/components/Loading/templateSkeleton.tsx
+++ src/components/Loading/templateSkeleton.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 export const TemplateSkeleton = () => (
   <div className="space-y-4">
-    <div className="h-8 bg-gray-200 rounded w-3/4"></div>
+    <div className="h-8 bg-gray-200 rounded w-3/4 animate-pulse"></div>
     <div className="space-y-2">
-      <div className="h-4 bg-gray-200 rounded w-full"></div>
-      <div className="h-4 bg-gray-200 rounded w-5/6"></div>
-      <div className="h-4 bg-gray-200 rounded w-4/6"></div>
+      <div className="h-4 bg-gray-200 rounded w-full animate-pulse"></div>
+      <div className="h-4 bg-gray-200 rounded w-5/6 animate-pulse"></div>
+      <div className="h-4 bg-gray-200 rounded w-4/6 animate-pulse"></div>
     </div>
-    <div className="h-32 bg-gray-200 rounded"></div>
+    <div className="h-32 bg-gray-200 rounded animate-pulse"></div>
     <div className="space-y-2">
-      <div className="h-4 bg-gray-200 rounded w-full"></div>
-      <div className="h-4 bg-gray-200 rounded w-5/6"></div>
+      <div className="h-4 bg-gray-200 rounded w-full animate-pulse"></div>
+      <div className="h-4 bg-gray-200 rounded w-5/6 animate-pulse"></div>
     </div>
   </div>
 );
